63cb05a267a27722ad66d0389094a73b0e5bcd9b
AnanyaSrivastava1203 <sriananya1203@gmail.com>
2025-01-06 15:25:26 +0530

    start api path

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const port = process.env.PORT||5000;
 app.use(express.json());
 app.use(cors());
 
-//mongodb username:sriananya1203
-//mongodb password:ISZHjWH84o2FtbTd
 
 main().catch((err) => console.log("MongoDB Connection Error:", err));
 
@@ -16,9 +14,6 @@ main().catch((err) => console.log("MongoDB Connection Error:", err));
 async function main() {
   await mongoose.connect('mongodb+srv://sriananya1203:ISZHjWH84o2FtbTd@veggify-react-app.et3sd.mongodb.net/veggify-react-app?retryWrites=true&w=majority&appName=veggify-react-app');
 
-  app.get('/', (req, res) => {
-    res.send('Veggify Recipe App Server is running!')
-  })
 }
 
 
@@ -29,6 +24,10 @@ const ItemRoutes=require("./src/routes/itemRoute");
 const CategoryRoutes=require("./src/routes/categoryRoute")
 app.use('/api',ItemRoutes)
 app.use('/api',CategoryRoutes)
+
+app.get('/', (req, res) => {
+  res.send('Veggify Recipe App Server is running!')
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
